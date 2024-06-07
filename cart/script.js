94869ef9f6560c398d0caeb3a9a44911bd4b9ba1
Daniel de Waal <danieldewaal02@gmail.com>
2024-06-07 06:58:15 +0200

    made cart more dynamic

--- cart/script.js
+++ cart/script.js
@@ -1,6 +1,7 @@
 function cart_item_oninput(event) {
+ if (event.srcElement.value == "") return;
  const quantity = parseInt(event.srcElement.value);
- console.log(quantity);
+ if (quantity == 0) return;
  set_cart_item_quantity(event.srcElement.id.split("-").at(-1), quantity);
 }
 function set_cart_item_quantity(product_listing_uuid, quantity) {
@@ -9,13 +10,20 @@ function set_cart_item_quantity(product_listing_uuid, quantity) {
  xhr.setRequestHeader("content-type", "application/json; charset=UTF-8");
  const body = JSON.stringify({
   product_listing_uuid: product_listing_uuid,
-  quantity: quantity
+  quantity: (quantity == NaN) ? 1 : quantity
   });
  xhr.onload = () => {
   console.log(xhr.responseText);
   const response = JSON.parse(xhr.responseText);
   if (response['status'] == 2) {
-   window.location.replace("login");
+   window.location.replace("/login");
+  } else {
+   let quantity_element = document.getElementById("cart-item-quantity-input-" + product_listing_uuid);
+   quantity_element.value = quantity;
+   let grandparent = quantity_element.parentNode.parentNode;
+   const price_element = grandparent.getElementsByClassName("cart-item-price")[0].getElementsByTagName("label")[0];
+   const price = parseFloat(price_element.textContent.split("$")[1]);
+   let total_price_element = grandparent.getElementsByClassName("cart-item-total-price")[0].getElementsByTagName("label")[0].textContent = "Total Cost: $" + (price * quantity).toFixed(2);
   }
  }
  xhr.send(body);
